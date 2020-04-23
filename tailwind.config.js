17913acafa25ac676647daf60c7303262c9bcab8
Maël Querré <mael.querre@gmail.com>
2020-04-23 17:23:03 +0200

    Add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        }
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const containers = {
+        '.container': {
+          width: '90%',
+          maxWidth: '1024px',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }
+      }
+
+      addUtilities(containers, ['responsive'])
+    })
+  ],
 }
