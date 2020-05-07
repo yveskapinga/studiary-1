0c08cb82496e047336689ef2f69aab0f92b9dea7
Maël Querré <mael.querre@gmail.com>
2020-05-07 13:21:20 +0200

    Update login template path

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,7 +20,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('pages/login.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     public function logout()
