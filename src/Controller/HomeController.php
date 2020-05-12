cb3582db72a8cca75353df85595e3fdfc832dc78
Maël Querré <mael.querre@gmail.com>
2020-05-12 21:07:22 +0200

    Remove global if

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,31 +16,26 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('login');
         }
 
-        // If the authenticated user is a teacher or a student,
-        // then render particular views
-        if (in_array('ROLE_TEACHER', $user->getRoles()) || in_array('ROLE_STUDENT', $user->getRoles())) {
+        if (in_array('ROLE_TEACHER', $user->getRoles())) {
+            $repository = $this->getDoctrine()->getRepository(Lesson::class);
+            $lessons = $repository->findLessonsByTeacherFromDate($user, new \DateTime());
 
-            if (in_array('ROLE_TEACHER', $user->getRoles())) {
-                $repository = $this->getDoctrine()->getRepository(Lesson::class);
-                $lessons = $repository->findLessonsByTeacherFromDate($user, new \DateTime());
+            return $this->render('pages/teacher_home.twig', [
+                'lessons' => $lessons
+            ]);
+        }
 
-                return $this->render('pages/teacher_home.twig', [
-                    'lessons' => $lessons
-                ]);
-            }
+        if (in_array('ROLE_STUDENT', $user->getRoles())) {
+            $repository = $this->getDoctrine()->getRepository(Lesson::class);
+            $lessons = $repository->findLessonsByGradeFromDate($user->getGrade(), new \DateTime());
 
-            if (in_array('ROLE_STUDENT', $user->getRoles())) {
-                $repository = $this->getDoctrine()->getRepository(Lesson::class);
-                $lessons = $repository->findLessonsByGradeFromDate($user->getGrade(), new \DateTime());
+            $repository = $this->getDoctrine()->getRepository(Note::class);
+            $notes = $repository->findNotesByUser($user);
 
-                $repository = $this->getDoctrine()->getRepository(Note::class);
-                $notes = $repository->findNotesByUser($user);
-
-                return $this->render('pages/student_home.twig', [
-                    'lessons' => $lessons,
-                    'notes' => $notes,
-                ]);
-            }
+            return $this->render('pages/student_home.twig', [
+                'lessons' => $lessons,
+                'notes' => $notes,
+            ]);
         }
 
         return $this->render('pages/home.twig');
