929f02d2972d287adb6d73bdbdc64f9bfc68bf41
Maël Querré <mael.querre@gmail.com>
2020-05-12 20:40:56 +0200

    Filter future events

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,17 +20,19 @@ class HomeController extends AbstractController
         // then render particular views
         if (in_array('ROLE_TEACHER', $user->getRoles()) || in_array('ROLE_STUDENT', $user->getRoles())) {
 
-            // We need lessons for both teacher and student
-            $repository = $this->getDoctrine()->getRepository(Lesson::class);
-            $lessons = $repository->findLessonsByUser($user);
-
             if (in_array('ROLE_TEACHER', $user->getRoles())) {
+                $repository = $this->getDoctrine()->getRepository(Lesson::class);
+                $lessons = $repository->findLessonsByTeacherFromDate($user, new \DateTime());
+
                 return $this->render('pages/teacher_home.twig', [
                     'lessons' => $lessons
                 ]);
             }
 
             if (in_array('ROLE_STUDENT', $user->getRoles())) {
+                $repository = $this->getDoctrine()->getRepository(Lesson::class);
+                $lessons = $repository->findLessonsByGradeFromDate($user, new \DateTime());
+
                 $repository = $this->getDoctrine()->getRepository(Note::class);
                 $notes = $repository->findNotesByUser($user);
 
